421557c095faab5a80beafad582febc232dcabc2
Soledad <soledad.lxj@gmail.com>
2022-03-14 13:53:32 +0100

    add: search bar

--- src/Component/Quote.js
+++ src/Component/Quote.js
@@ -2,22 +2,17 @@ import React, { useState } from 'react'
 import Social from './Social'
 import { FaQuoteLeft } from "react-icons/fa"
 import useConfig from '../useConfig'
-import Searchbar from './Searchbar'
-import quoteData from './quotes.json'
 import { getValue } from '@testing-library/user-event/dist/utils'
 import useSearch from './useSearch'
-const Quote = () => {
+import { BsSearch, BsX, BsCheck } from 'react-icons/bs'
+
+const Quote = ({ data }) => {
   // npm i randomquote package
   const Quotes = require("randomquote-api");
-
-
   const [quote, setQuote] = useState('Do something worth remembering.')
   const [author, setAuthor] = useState('Benjamin Franklin')
   const [bgImage, setBgImage] = useState('https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw1MzE3MnwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0NjQ5OTM1OA&ixlib=rb-1.2.1&q=85')
-
   const { clientID } = useConfig()
-
-  const getbyauthor = Quotes.getbyauthor("Stephen King", 1);
 
   function unsplashGetPhotos() {
     fetch(
@@ -35,18 +30,39 @@ const Quote = () => {
     setQuote(Quotes.randomQuote()['quote'])
     setAuthor(Quotes.randomQuote()['author'])
   }
-  // const authorData = quoteData.map(value => value.author)
-  // const authorSorted = authorData.sort((a, b) => a.localeCompare(b))
-  // const authorSet =  [...new Set(authorSorted)]; 
-
+  const [filteredData, dataInput, handleFilter, handleClear] = useSearch({ data });
 
   return (
     <div>
-      <Searchbar placeholder="Enter an author's name" data = {quoteData} />
+      <div className='search-container'>
+        <div className='search'>
+          <div className='searchInput' >
+            <div className='searchIcon'><BsSearch style={{ color: 'black' }} /></div>
+            <input type='text' placeholder='Write down the names' value={dataInput} onChange={handleFilter}></input>
+            <div className='crossIcon'>{dataInput.length == 0 ? <BsCheck style={{ color: 'black' }} />
+              : <BsX id='clearBtn' style={{ color: 'black' }} onClick={handleClear} />}
+            </div>
+          </div>
+          {filteredData.length != 0 && (
+            <div className='dataResult'>
+              {filteredData.slice(0, 10).map((value, key) => {
+                return (<a className='dataItem' href='#'>
+                  <p>{value}</p>
+                </a>)
+              })}
+            </div>
+          )
+          }
+        </div>
+      </div>
       {/* <ChangeColor tgChange = {handleChange}/> */}
       <div className='quote-bg-container' style={{ backgroundImage: `url(${bgImage})` }}>
-        <h1 className='quote'><FaQuoteLeft className='quote-icon' />{quote}</h1>
-        <h3 className='author-name'>{author}</h3>
+        <h1 className='quote'><FaQuoteLeft className='quote-icon' />
+          {filteredData[0] ? Quotes.getbyauthor(filteredData[0], 1)[0].quote : quote}
+        </h1>
+        <h3 className='author-name'>
+          {filteredData[0] ? Quotes.getbyauthor(filteredData[0], 1)[0].author : author}
+        </h3>
         <div className='social-container'>
           <button className='quote-button' onClick={handleNext}>New Quote</button>
           <Social quote={quote} author={author} />
